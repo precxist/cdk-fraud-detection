97caf367fe68b0db2705c422ca964f8d8a92dced
minjkang <minjkang@amazon.com>
2019-10-14 05:28:25 +0000

    added untracked

---
+++ lib/lambda/s3_client.py
@@ -0,0 +1,18 @@
+import os
+import time
+import json
+
+import boto3
+
+
+SCORE_BUCKET_NAME = os.environ['SCORE_BUCKET_NAME']
+
+
+def save_result_to_s3(transaction_list):
+    s3 = boto3.resource("s3")
+    score_bucket = s3.Bucket(SCORE_BUCKET_NAME)
+    ts = str(int(time.time() * 1e7))
+    result_file_name = ts + ".json"
+    json_str = json.dumps(transaction_list)
+    score_bucket.put_object(Key=result_file_name, Body=json_str)
+    
